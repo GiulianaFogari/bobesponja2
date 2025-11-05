e420cd74ca73bea9e46507313f7909dbb3ef029a
GabrielDias0103 <gsdias2014@gmail.com>
2025-11-04 22:06:16 -0300

    Usuario Atual

---
+++ Models/UsuarioAtual.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static bobesponja2._0.Models.Usuario;
+
+namespace bobesponja2._0.Models
+{
+    public sealed class UsuarioAtual
+    {
+        private static UsuarioAtual instance = new UsuarioAtual();
+        private UsuarioAtual() { }
+
+        public static UsuarioAtual Instance => instance;
+
+        public Usuario Usuario { get; private set; }
+
+        public void SetUsuario(Usuario u) => Usuario = u;
+        public void Logout() => Usuario = null;
+        public bool IsAdmin => Usuario != null && Usuario.Tipo == TipoUsuario.Administrador;
+    }
+}
