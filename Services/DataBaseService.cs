3030a721df6234d44d27149ccb81170c8796b2b4
GiulianaFogari <giulianaalvesfogarialvesfogari@gmail.com>
2025-11-04 22:36:38 -0300

    alterações na db e criado a viewmodel de item

--- Services/DataBaseService.cs
+++ Services/DataBaseService.cs
@@ -1,26 +1,84 @@
 ﻿using SQLite;
 using PCLExt.FileStorage.Folders;
+using bobesponja2._0.Models;
 
 namespace bobesponja2._0.Services
 {
     public class DataBaseService
     {
 
-        public SQLiteConnection GetConexao()
+        private SQLiteAsyncConnection _db;
+
+        public DataBaseService()
         {
+            var folder = new LocalRootFolder();
+            var file = folder.CreateFile("bobburg.db",
+                PCLExt.FileStorage.CreationCollisionOption.OpenIfExists);
 
-            var folder =
-                new LocalRootFolder();
+            _db = new SQLiteAsyncConnection(file.Path);
+        }
 
-         
-            var file =
-                folder.CreateFile("bobburg",
-                    PCLExt.FileStorage.
-                        CreationCollisionOption.
-                            OpenIfExists);
+        // Inicializa as tabelas
+        public async Task InitAsync()
+        {
+            await _db.CreateTableAsync<Usuario>();
+            await _db.CreateTableAsync<Item>();
+            await _db.CreateTableAsync<Pedido>();
+        }
 
-            return
-                new SQLiteConnection(file.Path);
+        public async Task<int> AddUsuarioAsync(Usuario usuario)
+        {
+            return await _db.InsertAsync(usuario);
+        }
+
+        public async Task<Usuario> GetUsuarioByEmailSenhaAsync(string email, string senha)
+        {
+            return await _db.Table<Usuario>()
+                .Where(usuario => usuario.Email == email && usuario.Senha == senha)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Usuario>> GetUsuariosAsync()
+        {
+            return await _db.Table<Usuario>().ToListAsync();
+        }
+
+        public async Task<int> AddItemAsync(Item item)
+        {
+            return await _db.InsertAsync(item);
+        }
+
+        public async Task<int> UpdateItemAsync(Item item)
+        {
+            return await _db.UpdateAsync(item);
+        }
+
+        public async Task<int> DeleteItemAsync(Item item)
+        {
+            return await _db.DeleteAsync(item);
+        }
+
+        public async Task<List<Item>> GetItensAsync()
+        {
+            return await _db.Table<Item>().ToListAsync();
+        }
+
+     
+        public async Task<int> AddPedidoAsync(Pedido pedido)
+        {
+            return await _db.InsertAsync(pedido);
+        }
+
+        public async Task<List<Pedido>> GetPedidosAsync()
+        {
+            return await _db.Table<Pedido>().ToListAsync();
+        }
+
+        public async Task<List<Pedido>> GetPedidosByUsuarioAsync(int usuarioId)
+        {
+            return await _db.Table<Pedido>()
+                .Where(pedido => pedido.UsuarioId == usuarioId)
+                .ToListAsync();
         }
     }
 }
