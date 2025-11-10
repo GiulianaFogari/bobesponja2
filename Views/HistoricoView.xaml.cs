fe993b210b6aedd736dcae190e0f304f3cadc880
GiulianaFogari <giulianaalvesfogarialvesfogari@gmail.com>
2025-11-10 19:41:06 -0300

    alterações finais

--- Views/HistoricoView.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace bobesponja2._0.Views;
-
-public partial class HistoricoView : ContentPage
-{
-	public HistoricoView()
-	{
-		InitializeComponent();
-	}
-}
--- Views/ItemView.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace bobesponja2._0.Views;
-
-public partial class ItemView : ContentPage
-{
-	public ItemView()
-	{
-		InitializeComponent();
-	}
-}
